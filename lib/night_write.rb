6ce1bb6ef571db43cf0d1e47394423dcf7ab69a8
Jack Pincus <jwpincus@gmail.com>
2017-02-03 16:55:05 -0700

    Got File IO working

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -1,14 +1,24 @@
 class Brailler
     def initialize(filename = ARGV[0], savefilename = ARGV[1])
         @filename = filename
-        @savefilename = savefilename
+        @savefilename = './lib/braille.txt'
+        @english_input = ''
+        @braille_out = ''
     end
 
-    def file_in
-        english_input = File.open(@filename).read
-        puts english_input
+    def file_open
+        @english_input = File.open(@filename).read
+        @english_input
+    end
+
+    def file_write
         
+        braille = File.open(@savefilename, 'w')
+        braille.write(@english_input)
     end
 
+    def parse_to_translator
+        @english_input 
+    end
 end
 
--- test/test_night_write.rb
+++ test/test_night_write.rb
@@ -3,12 +3,21 @@ require './lib/night_write'
 require 'minitest/pride'
 
 class NightWriteTest < Minitest::Test
+
     def test_file_IO_object_exists
         file = Brailler.new('message.txt')
         assert Brailler.new('message.txt')
     end
-    def test_file_in_out
+
+    def test_file_in
         file = Brailler.new('./lib/message.txt')
-        file.file_in
+        assert_equal file.file_open.length, 255
     end
+    
+    def test_file_out
+        file = Brailler.new('./lib/message.txt')
+        file.file_open
+        file.file_write
+    end
+
 end
