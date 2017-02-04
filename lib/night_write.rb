9259eaaba3c1f80b84f92de031b5d90e42f6b458
Jack Pincus <jwpincus@gmail.com>
2017-02-04 12:10:09 -0700

    pulling in nich's branch

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -1,26 +1,27 @@
-class Brailler
+class ReadWrite
 
     attr_reader :text_input
     attr_accessor :text_out
 
-    def initialize(filename = ARGV[0], savefilename = ARGV[1])
+    def initialize(filename = ARGV[0], savefilename = ARGV[1], text_input = '', @text_out = '')
         @filename = filename
         @savefilename = savefilename
-        @text_input = ''
+        @text_input = text_input
         @text_out = ''
     end
 
-    def file_open(file_to_open = @filename)
-        @text_input = File.open(file_to_open).read
+    def file_open
+        @text_input = File.open(@filename).read
         @text_input
     end
 
-    def file_write(string_to_write = @text_out)       
+    def file_write(text_to_write = @text_out)
         braille = File.open(@savefilename, 'w')
-        braille.write(string_to_write)
-        puts "Created '#{@savefilename}' containing #{@text_out.length} characters"
+        braille.write(text_to_write)
+        puts "Created '#{@savefilename}' containing #{text_to_write.length} characters"
     end
 
 end
+
 
 
